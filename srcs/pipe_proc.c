f503bda46ca9e46bc9141da20133f87b6b844ccc
ejei0g@gmail.com <ejei0g@gmail.com>
2020-12-13 20:49:10 +0900

    flag

--- srcs/pipe_proc.c
+++ srcs/pipe_proc.c
@@ -12,7 +12,6 @@ static int	printf_err()
 
 int	pipe_process(t_stock_str *ms, t_env_list **head, char *line)
 {
-	printf("in pipe\n");
 	int	fd[2];
 	pid_t	pid;
 
@@ -20,10 +19,6 @@ int	pipe_process(t_stock_str *ms, t_env_list **head, char *line)
 		return (printf_err());
 	if ((pid = fork()) < 0)
 		return (printf_err());
-	printf("forked%d,%d\n",fd[0], fd[1]);
-	//printf("pid = %d", pid);
-	ft_putnbr_fd(pid, 1);
-	write(1, "\n", 1);
 	if (pid > 0)
 	{
 	//	wait(&status);
@@ -31,24 +26,17 @@ int	pipe_process(t_stock_str *ms, t_env_list **head, char *line)
 
 		str_init(ms);
 		parsing(line, ms, *head);
-		printf("error\n");
 		dup2(fd[READ], STDIN_FILENO);
-		printf("after dup2\n");
-		printf("p_flag = %d\n", ms->p_flag);
 		if (ms->p_flag)
 		{
-			printf("in second pipe\n");
+		//	printf("in second pipe\n");
 			pipe_process(ms, head, line);
 		}
 		else
 		{
-			printf("start msproc \n");
-			printf("fd = %d\n", fd[0]);
-			printf("args = %s\n", ms->args[0]);
-			dup2(ms->fd_outorg, STDOUT_FILENO);
-			execve("/bin/grep", ms->args, 0);
-			//ms_proc(*ms, head);
-			printf("after msproc \n");
+		//	dup2(ms->fd_outorg, STDOUT_FILENO);
+//			execve("/bin/grep", ms->args, 0);
+			ms_proc(*ms, head);
 
 		}
 		//원상복귀
@@ -62,7 +50,7 @@ int	pipe_process(t_stock_str *ms, t_env_list **head, char *line)
 		dup2(fd[WRITE], STDOUT_FILENO);
 		ms_proc(*ms, head);
 		//원상복귀
-		//dup2(ms->fd_outorg, STDOUT_FILENO);
+		dup2(ms->fd_outorg, STDOUT_FILENO);
 		printf("stdout change test\n");
 	}
 	return (0);
