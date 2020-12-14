7d3785b8b48029029e31ee8efcfef2760a651391
jklog93yhj <jklog93yhj@naver.com>
2020-12-14 16:25:50 +0900

    parsing_norm

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -1,7 +1,18 @@
 #include "minishell.h"
 
 //TODO echo "$_" 나중에 하기
-void	argv_parsing(t_stock_str *ms, int i);
+// $1 asd 이렇게 했을떄 $1이 널값이면 앞에 스페이스가 추가되는거 수정해야됨
+void	argv_parsing(t_stock_str *ms, int i)
+{
+	int j;
+
+	j = 0;
+	if (ms->argv[i] == NULL)
+		return ;
+	while (ms->argv[i][j])
+		ms->args[ms->h][ms->w++] = ms->argv[i][j++];
+	return ;
+}
 
 int	flag_check(t_stock_str *ms, char c)
 {
@@ -13,6 +24,7 @@ int	flag_check(t_stock_str *ms, char c)
 		return (1);
 	return (0);
 }
+
 void	str_init(t_stock_str *ms)
 {
 	ms->w = 0; //args_width
@@ -41,108 +53,118 @@ int	space_check(char *line)
 	}
 	return (j);
 }
-void	dollor_parsing(t_stock_str *ms, char *line, t_env_list *head)
-{
-	int brace;
-	int i;
-	int j;
-	char a[100];
 
-	brace = 0;
-	i = 0;
-	j = 0;
-	ms->l_idx++;
-	if (line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9')
-	{
-		i = line[ms->l_idx++] - 48;
-		ms->l_idx--;
-		argv_parsing(ms, i);
-		return ;
-	}
-	else if (line[ms->l_idx] == '#')
-	{
-		ms->args[ms->h][ms->w++] = ms->argc + 48 - 1;
-		return ;
-	}
+int		all_argv_parsing(t_stock_str *ms, char *line)
+{
+	int i;
+
 	i = 1;
 	if (line[ms->l_idx] == '*' || line[ms->l_idx] == '@')
 	{
 		while (i < ms->argc)
 		{
-			argv_parsing(ms, i);
-			i++;
+			argv_parsing(ms, i++);
 			if (ms->argv[i] == NULL)
-				return ;
-			ms->args[ms->h][ms->w] = '\0';
-			ms->h++;
+			{
+				ms->l_idx++;
+				return 0;
+			}
+			ms->args[ms->h++][ms->w] = '\0';
 			ms->w = 0;
 		}
-		return ;
+		ms->l_idx++;
+		return 0;
 	}
+	return 1;
+}
+
+int		dollor_argcv(t_stock_str *ms, char *line, int i)
+{
+	if (line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9')
+	{
+		while (line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9')
+			i = i * 10 + line[ms->l_idx++] - 48;
+		argv_parsing(ms, i);
+		return 0;
+	}
+	else if (line[ms->l_idx] == '#')
+	{
+		ms->args[ms->h][ms->w++] = ms->argc + 48 - 1;
+		return 0;
+	}
+	if (all_argv_parsing(ms, line) == 0)
+		return (0);
+	return (1);
+}
+
+int		dollor_parsing2(t_stock_str *ms, char *line, int brace)
+{
 	if (line[ms->l_idx] == '{')
 	{
 		ms->l_idx++;
 		brace = 1;
 	}
-	else if (!((line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9') || (line[ms->l_idx] >= 'a' && line[ms->l_idx] <= 'z') || (line[ms->l_idx] >= 'A' && line[ms->l_idx] <= 'Z') || line[ms->l_idx] == '_'))
+	if (dollor_argcv(ms, line, 0) == 0)
 	{
-		ms->args[ms->h][ms->w] = '$';
-		ms->w++;
+		if (brace != 1) // echo ${0asd} 에러처리
+			ms->l_idx--;
+		return -1;
+	}
+	if (!((line[ms->l_idx] >= 'a' && line[ms->l_idx] <= 'z') || (line[ms->l_idx] >= 'A' && line[ms->l_idx] <= 'Z') || line[ms->l_idx] == '_'))
+	{
+		ms->args[ms->h][ms->w++] = '$';
 		ms->l_idx--;
-		return ;
+		return -1;
 	}
-	if (line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9')
-	{
-		i = 0;
-		while (line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9')
-			i = i * 10 + line[ms->l_idx++] - 48;
-		argv_parsing(ms, i);
-	}
-	if (line[ms->l_idx] == '*' || line[ms->l_idx] == '@')
-        {
-		i = 0;
-                while (i < ms->argc)
-                {
-                        argv_parsing(ms, i);
-                        i++;
-                        if (ms->argv[i] != NULL)
-			{
-                        ms->args[ms->h][ms->w] = '\0';
-                        ms->h++;
-                        ms->w = 0;
-			}
-                }
-        }
-	while ((line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9') || (line[ms->l_idx] >= 'a' && line[ms->l_idx] <= 'z') || (line[ms->l_idx] >= 'A' && line[ms->l_idx] <= 'Z') || line[ms->l_idx] == '_')
-	{
-		a[j] = line[ms->l_idx];
-		j++;
-		ms->l_idx++;
-	}
-	if (brace == 1)
-	{
-		while (line[ms->l_idx] != '}')
-			ms->l_idx++;
-		if (line[ms->l_idx] == '}')
-			ms->l_idx++;
-	}
-	a[j] = '\0';
+	return (brace);
+}
+
+void	env_parsing(t_stock_str *ms, t_env_list *head, char a[100])
+{
+	int i;
+
+	i = 0;
 	t_env_list *test;
 	test = find_env_key(&head, a);
 	if (test != NULL)
 	{
-		int hwyu = 0;
-		while (test->data[hwyu] != '=')
-			hwyu++;
-		hwyu++;
-		while (test->data[hwyu])
+		while (test->data[i] != '=')
+			i++;
+		i++;
+		while (test->data[i])
 		{
-			ms->args[ms->h][ms->w] = test->data[hwyu];
+			ms->args[ms->h][ms->w] = test->data[i];
 			ms->w++;
-			hwyu++;
+			i++;
 		}
 	}
 	ms->l_idx--;
+}
+
+void	dollor_parsing(t_stock_str *ms, char *line, t_env_list *head)
+{
+	int brace;
+	int j;
+	char a[100];
+
+	brace = 0;
+	j = 0;
+	ms->l_idx++;
+	brace = dollor_parsing2(ms, line, brace);
+	if (brace == -1)
+		return ;
+	while ((line[ms->l_idx] >= '0' && line[ms->l_idx] <= '9') ||
+			(line[ms->l_idx] >= 'a' && line[ms->l_idx] <= 'z') ||
+			(line[ms->l_idx] >= 'A' && line[ms->l_idx] <= 'Z') || line[ms->l_idx] == '_')
+		a[j++] = line[ms->l_idx++];
+	a[j] = '\0';
+	if (brace == 1)
+	{
+		while (line[ms->l_idx] != '}')
+			ms->l_idx++;
+		ms->l_idx++;
+	}
+	env_parsing(ms, head, a);
 }
 
 void	sq_flag_parsing(t_stock_str *ms, char *line)
@@ -170,18 +192,6 @@ void	dq_flag_parsing(t_stock_str *ms, char *line, t_env_list *head)
 			ms->args[ms->h][ms->w++] = line[ms->l_idx++];
 	}
 	ms->dq_flag = 0;
-}
-
-void	argv_parsing(t_stock_str *ms, int i)
-{
-	int j;
-
-	j = 0;
-	if (ms->argv[i] == NULL)
-		return ;
-	while (ms->argv[i][j])
-		ms->args[ms->h][ms->w++] = ms->argv[i][j++];
-	return ;
 }
 
 int	null_flag(t_stock_str *ms, char *line)
@@ -215,34 +225,52 @@ int	null_flag(t_stock_str *ms, char *line)
 	return (-1);
 }
 
+int		rdir_flag_setting(t_stock_str *ms, char *line)
+{
+	if (line[ms->l_idx] == '>' && line[ms->l_idx + 1] == '>')
+	{
+		ms->l_idx = ms->l_idx + 2;
+		return (1);
+	}
+	else if (line[ms->l_idx] == '>')
+	{
+		ms->l_idx = ms->l_idx + 1;
+		return (2);
+	}
+	else if (line[ms->l_idx] == '<')
+	{
+		ms->l_idx = ms->l_idx + 1;
+		return (3);
+	}
+	return (0);
+}
+
+void	rdir_fd_dup(t_stock_str *ms, int rdir_flag)
+{
+	int	fd = 0;
+
+	if (rdir_flag == 1)
+		fd = open(ms->file_name, O_WRONLY | O_CREAT | O_APPEND);
+	else if (rdir_flag == 2)
+		fd = open(ms->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (rdir_flag == 3)
+		fd = open(ms->file_name, O_RDONLY);
+	if (rdir_flag == 3)
+		dup2(fd, STDIN_FILENO);
+	else
+		dup2(fd, STDOUT_FILENO);
+}
+
 void	redirect_parsing(t_stock_str *ms, char *line)
 {
 	int	i = 0;
-	int	fd = 0;
-//	int	fd_copy;
-	int	rdir_flag = 0;
+	int	rdir_flag;
 	char	filename[100];
 	int	cp_l_idx;
 
 	i = 0;
 	cp_l_idx = 0;
-	printf("redirecion\n");
-//	fd_copy = fd;
-	if (line[ms->l_idx] == '>' && line[ms->l_idx + 1] == '>')
-	{
-		rdir_flag = 1;
-		ms->l_idx = ms->l_idx + 2;
-	}
-	else if (line[ms->l_idx] == '>')
-	{
-		ms->l_idx = ms->l_idx + 1;
-		rdir_flag = 2;
-	}
-	else if (line[ms->l_idx] == '<')
-	{
-		ms->l_idx = ms->l_idx + 1;
-		rdir_flag = 3;
-	}
+	rdir_flag = rdir_flag_setting(ms, line);
 	while (line[ms->l_idx] == ' ' && line[ms->l_idx] != '\0')
 		ms->l_idx++;
 	cp_l_idx = ms->l_idx;
@@ -252,38 +280,70 @@ void	redirect_parsing(t_stock_str *ms, char *line)
 	ms->file_name = ft_strdup(filename);
 	if (ft_strncmp(ms->args[0], "echo", 4) == 0)
 		return ;
-	if (rdir_flag == 1)
-		fd = open(ms->file_name, O_WRONLY | O_CREAT | O_APPEND);
-	if (rdir_flag == 2)
-		fd = open(ms->file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else if (rdir_flag == 3)
-		fd = open(ms->file_name, O_RDONLY);
+	rdir_fd_dup(ms, rdir_flag);
 	if (rdir_flag == 3)
-	{
-		dup2(fd, STDIN_FILENO);
 		ms->l_idx = cp_l_idx;
-	}
-	else
-		dup2(fd, STDOUT_FILENO);
-	printf("cp = %d\n", cp_l_idx);
 	ms->l_idx--;
 }
 
-int	parsing(char *line, t_stock_str *ms, t_env_list *head)
+int		p_sc_flag_parsing(t_stock_str *ms, char *line)
 {
-	int i;
-	int j;
-	i = 0;
-	j = 1 + space_check(line);
-	ms->args = (char **)malloc(sizeof(char *) * (j + 1 + ms->argc));
-	while (i < j + ms->argc)
-		ms->args[i++] = (char *)malloc(sizeof(char) * 1000);
-	i = 0;
+	if (line[ms->l_idx] == '|')
+	{
+		ms->p_flag = 1;
+		ms->l_idx++;
+		while (line[ms->l_idx] == ' ')
+			ms->l_idx++;
+		return (0);
+	}
+	else if (line[ms->l_idx] == ';')
+	{
+		ms->sc_flag = 1;
+		ms->l_idx++;
+		while (line[ms->l_idx] == ' ')
+			ms->l_idx++;
+		return (0);
+	}
+	return (-1);
+}
+
+int	parsing_n_apace(t_stock_str *ms, char *line, t_env_list *head)
+{
+	if (line[ms->l_idx] == '\\')
+		ms->args[ms->h][ms->w++] = line[++ms->l_idx];
+	else if (p_sc_flag_parsing(ms, line) == 0)
+		return (-1);
+	else if (line[ms->l_idx] == '$')
+		dollor_parsing(ms, line, head);
+	else if (line[ms->l_idx] == '>' || line[ms->l_idx] == '<')
+		redirect_parsing(ms, line);
+	else
+		ms->args[ms->h][ms->w++] = line[ms->l_idx];
+	return (0);
+}
+
+void	parsing_is_space(t_stock_str *ms, char *line)
+{
+	ms->args[ms->h][ms->w] = '\0';
+	while (line[ms->l_idx + 1] == ' ')
+		ms->l_idx++;
+	while (null_flag(ms, line) == 0)
+		;
+	while (line[ms->l_idx + 1] == ' ')
+		ms->l_idx++;
+	if (line[ms->l_idx + 1] == '|' || line[ms->l_idx + 1] == ';' || line[ms->l_idx + 1] == '>')
+		;
+	else if (line[ms->l_idx + 1] != '\0')
+	{
+		ms->w = 0;
+		ms->h++;
+	}
+}
+
+void	line_parsing(char *line, t_stock_str *ms, t_env_list *head)
+{
 	while (line[ms->l_idx])
 	{
-	//	printf("line = %c\n", line[ms->l_idx]);
-	//	if (null_flag(ms, line) == 0)
-	//		;
 		if (ms->sq_flag == 1)
 			sq_flag_parsing(ms, line);
 		else if (ms->dq_flag == 1)
@@ -292,60 +352,27 @@ int	parsing(char *line, t_stock_str *ms, t_env_list *head)
 			;
 		else if (line[ms->l_idx] != ' ')
 		{
-			if (line[ms->l_idx] == '\\')
-			{
-				ms->l_idx++;
-				ms->args[ms->h][ms->w] = line[ms->l_idx];
-				ms->w++;
-			}
-			else if (line[ms->l_idx] == '|')
-			{
-				ms->p_flag = 1;
-				ms->l_idx++;
-				while (line[ms->l_idx] == ' ')
-					ms->l_idx++;
-				break ;
-			}
-			else if (line[ms->l_idx] == ';')
-			{
-				ms->sc_flag = 1;
-				ms->l_idx++;
-				while (line[ms->l_idx] == ' ')
-					ms->l_idx++;
-				break ;
-			}
-			else if (line[ms->l_idx] == '$')
-				dollor_parsing(ms, line, head);
-			else if (line[ms->l_idx] == '>' || line[ms->l_idx] == '<')
-			{
-				write(1, "1q2w3e4r\n", 9);
-				redirect_parsing(ms, line);
-			}
-			else
-			{
-				ms->args[ms->h][ms->w] = line[ms->l_idx];
-				ms->w++;
-			}
+			if (parsing_n_apace(ms, line, head) == -1)
+				break;
 		}
 		else if (line[ms->l_idx] == ' ')
-		{
-			ms->args[ms->h][ms->w] = '\0';
-			while (line[ms->l_idx + 1] == ' ')
-				ms->l_idx++;
-			while (null_flag(ms, line) == 0)
-				;
-			while (line[ms->l_idx + 1] == ' ')
-				ms->l_idx++;
-			if (line[ms->l_idx + 1] == '|' || line[ms->l_idx + 1] == ';' || line[ms->l_idx + 1] == '>')
-				;
-			else if (line[ms->l_idx + 1] != '\0')
-			{
-				ms->w = 0;
-				ms->h++;
-			}
-		}
+			parsing_is_space(ms, line);
 		ms->l_idx++;
 	}
+}
+
+int	parsing(char *line, t_stock_str *ms, t_env_list *head)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 1 + space_check(line);
+	ms->args = (char **)malloc(sizeof(char *) * (j + 1 + ms->argc));
+	while (i < j + ms->argc)
+		ms->args[i++] = (char *)malloc(sizeof(char) * 1000);
+	i = 0;
+	line_parsing(line, ms, head);
 	ms->args_cnt = ms->h;
 	ms->args[ms->h][ms->w] = '\0';
 	ms->last_args = ft_strdup(ms->args[ms->h]);
